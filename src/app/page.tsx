e230bd98108470ed8ae9fddf98235faea9698e18
notificacion-he <notificacionhe5@gmail.com>
2025-09-26 14:06:03 +0000

    No sige sin aparecer el menu lateral

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-
-import Home from './(app)/page';
-
-// Exporta el componente del dashboard para que sea la página de inicio.
-// Al importar desde la ruta '(app)', nos aseguramos de que se renderice dentro del layout principal.
-export default Home;
